015b67eb23d9f42ae09c9114876e00e82b70f5cb
Robert Piotrowski <50244265+revoxhere@users.noreply.github.com>
2020-06-17 22:50:20 +0200

    V2 - code cleanup and english comments

--- smarthome.ino
+++ smarthome.ino
@@ -1,40 +1,31 @@
-//biblioteki
+//Smart Sockets v2 system for ESP boards by revox
+//libraries
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-#include "mainPage.h" //dolaczamy przygotowana strone
+#include "mainPage.h" //add webpage
 
-//definiujemy piny wyjscia
-const int Load1 = 16; //load1
-const int Load2 = 14; //load 2
-const int Load3 = 12; //load 3
-const int Load4 = 13; //load 4
-const int Load5 = 4; //load 5
-const int Load6 = 5; //load 6, uzywane tu jako pin pwm
+const char *ssid = "ssid"; //wifi ssif
+const char *password = "pass"; //wifi pass
 
-const char *ssid = "ssid"; //nazwa sieci
-const char *password = "pass"; //haslo do wifi
+//input pins
+const int Load1 = 16;
+const int Load2 = 14;
+const int Load3 = 12; 
+const int Load4 = 13; 
+const int Load5 = 4; 
+const int Load6 = 5; 
 
-//sprawy od pwm
-int brightness = 0; 
-int fadeAmount = 5;  
-unsigned long currentTime;
-unsigned long loopTime;
-int fade;
-
-//zmienne i esp nasluchuje na porcie 80
-ESP8266WebServer server(80);
 String L1Status, L2Status, L3Status, L4Status, Temperature, L5Status, L6Status;
+ESP8266WebServer server(80);
 
-//zmiana @@dane@@ na realne dane w stronie
 void handleRoot() {
   String s = MAIN_page;
-  s.replace("@@L1@@", L1Status);
+  s.replace("@@L1@@", L1Status); //variables for web - replace @@xx@@ for switch state
   s.replace("@@L2@@", L2Status);
   s.replace("@@L3@@", L3Status);
   s.replace("@@L4@@", L4Status);
-  s.replace("@@TEMP@@", Temperature);
   s.replace("@@L5@@", L5Status);
   s.replace("@@L6@@", L6Status);
   server.send(200, "text/html", s);
@@ -43,125 +34,99 @@ void handleRoot() {
 void handleForm() {
   String t_state = server.arg("submit");
 
-  Temperature = 20; //jeszcze do zrobienia
-
   if (t_state == "ON1")
   {
-    L1Status = "(Wlaczone)";
+    L1Status = "(ON)";
     digitalWrite(Load1, HIGH);
   }
 
   if (t_state == "OFF1")
   {
-    L1Status = "(Wylaczone)";
+    L1Status = "(OFF)";
     digitalWrite(Load1, LOW);
   }
 
   if (t_state == "ON2")
   {
-    L2Status = "(Wlaczone)";
+    L2Status = "(ON)";
     digitalWrite(Load2, HIGH);
   }
 
   if (t_state == "OFF2")
   {
-    L2Status = "(Wylaczone)";
+    L2Status = "(OFF)";
     digitalWrite(Load2, LOW);
   }
 
   if (t_state == "ON3")
   {
-    L3Status = "(Wlaczone)";
+    L3Status = "(ON)";
     digitalWrite(Load3, HIGH);
   }
 
   if (t_state == "OFF3")
   {
-    L3Status = "(Wylaczone)";
+    L3Status = "(OFF)";
     digitalWrite(Load3, LOW);
   }
 
   if (t_state == "ON4")
   {
-    L4Status = "(Wlaczone)";
+    L4Status = "(ON)";
     digitalWrite(Load4, HIGH);
   }
 
   if (t_state == "OFF4")
   {
-    L4Status = "(Wylaczone)";
+    L4Status = "(OFF)";
     digitalWrite(Load4, LOW);
   }
 
   if (t_state == "ON5")
   {
-    L5Status = "(Wlaczone)";
+    L5Status = "(ON)";
     digitalWrite(Load5, HIGH);
   }
 
   if (t_state == "OFF5")
   {
-    L5Status = "(Wylaczone)";
+    L5Status = "(OFF)";
     digitalWrite(Load5, LOW);
   }
 
-
   if (t_state == "ON6")
   {
-    L6Status = "(Wlaczone)";
-    fade = 1; //ustaw fade na 1 czyli wlacz pwm
+    L6Status = "(ON)";
   }
 
   if (t_state == "OFF6")
   {
-    L6Status = "(Wylaczone)";
-    fade = 0;
-    digitalWrite(Load6, LOW);
+    L6Status = "(OFF)";
   }
-  //dzieki temu wszystko jest na jednej stronie
+
+  //everything is on single page thanks to this redirect
   server.sendHeader("Location", "/");
   server.send(302, "text/plain", "Updated-- Press Back Button");
-
-  delay(500);
+  delay(100);
 }
 
 void setup() {
-  delay(1000);
-  WiFi.mode(WIFI_AP_STA); //konf. wifi
+  delay(500);
+  WiFi.mode(WIFI_AP_STA); //wifi config
   WiFi.begin(ssid, password);
   IPAddress myIP = WiFi.softAPIP();
   server.on("/", handleRoot);
   server.on("/form", handleForm);
+  server.begin();
 
-  server.begin(); //konf. pinow
-  pinMode(Load1, OUTPUT);
+  pinMode(Load1, OUTPUT); //pin configurations
   pinMode(Load2, OUTPUT);
   pinMode(Load3, OUTPUT);
   pinMode(Load4, OUTPUT);
   pinMode(Load5, OUTPUT);
   pinMode(Load6, OUTPUT);
-  currentTime = millis(); //sprawy od pwm
-  loopTime = currentTime;
 }
 
-
 void loop() {
-  server.handleClient(); //niech esp obsluzy klienta
-
-  //Load 6 bedzie pinem PWM i robic powoli on-off
-  if (fade == 1)
-  {
-    L6Status = "(Wlaczone)";
-
-    currentTime = millis();
-    if (currentTime >= (loopTime + 20)) {
-      analogWrite(Load6, brightness);
-      brightness = brightness + fadeAmount;
-      if (brightness == 0 || brightness == 255) {
-        fadeAmount = -fadeAmount ;
-      }
-      loopTime = currentTime;
-    }
-  }
-  
+  server.handleClient(); //let ESP handle clients
 }
